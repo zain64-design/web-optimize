27876ccad1efc8589b5912f74620a00f51dc80b9
zain64-design <zainyzain64@gmail.com>
2026-04-18 05:04:30 +0500

    new changes

--- index-check.php
+++ index-check.php
@@ -4,15 +4,20 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Book Publishers | Publish Your Book with Expert Support</title>
     <link rel="dns-prefetch" href="//cdn.jsdelivr.net">
     <link rel="dns-prefetch" href="//cdnjs.cloudflare.com">
     <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon-icon.png">
-    <?php  include 'includes/tags/tags.php';?>
+    <meta name="keywords" content="book publishers, publish my book, self publishing, best book publishers for new authors, self publishing a book, book publishing companies, publish a story, publish your book, novella publishers, publishers in usa, get my story published, childrens book publishers">
+    <meta name="description" content="Professional book publishers offering publishing services like editing, cover design, and distribution to help you publish your book.">
     <?php  include 'includes/sections/variables-file.php';?>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <!-- banner preload -->
     <link rel="preload" as="image" href="assets/images/banner-publishing-bg.webp" fetchpriority="high">
+        <style>
+.banner-inner-published-form .form-group .form-select,.banner-inner-published-form .form-group textarea{font-family:Poppins,sans-serif;background-color:#fbfbfb;outline:0;box-shadow:none;width:100%;font-size:16px}body{font-family:Poppins,sans-serif}header{background:#fff;width:100%;box-shadow:0 0 20px -5px #b2b2b2;position:fixed;top:0;z-index:1000;transition:.3s ease-in-out}header nav.navbar{padding:10px 0;align-items:center;position:inherit}header nav.navbar .navbar-brand{padding:0;margin:0}header nav.navbar .navbar-brand .logo{max-width:100%;height:45px;object-fit:contain;margin:0;transition:.5s ease-in-out;display:block}header nav.navbar .website-main-buttons{justify-content:center;margin:0;gap:10px}header nav.navbar .website-main-buttons a.call-btn{background:0 0;color:#161a1d;border:2px solid #8340ee;border-radius:5px;margin:0}.banner-main-content .website-main-buttons button.chat-btn:hover{background:#8340ee;color:#fff;transform:scale(.9);border-color:#8340ee}header nav.navbar .website-main-buttons a.call-btn i{margin:0 0 0 5px;transform:rotate(-45deg)}.banner-main-sec{padding:160px 0 50px;position:relative;z-index:1;background:#000}.banner-main-sec .bg-img{position:absolute;top:0;left:0;right:0;bottom:0;z-index:-1}.banner-main-sec .bg-img .banner-img{object-fit:cover;object-position:center;height:100%;width:100%}.banner-main-content .heading-one{color:#fff;margin:0 0 20px;text-align:left}.banner-main-content p{color:#fff;margin:0 0 10px;text-align:left}.banner-main-content .website-main-buttons{margin:30px 0 0;gap:10px}.banner-main-content .website-main-buttons button.chat-btn{background:0 0;color:#fff;border:2px solid #8340ee;border-radius:5px}.banner-inner-published-form{background:#fff;width:100%;border-radius:0;box-shadow:0 0 15px -5px #fff;padding:20px;margin:0 auto}.banner-inner-published-form .form-group{position:relative;padding:0 0 20px}.banner-inner-published-form .form-group textarea{padding:15px 10px 10px;color:#000;border:2px solid #e9e7e7;height:100px;border-radius:0;resize:none}.banner-inner-published-form .form-group button.submit-btn:hover{transform:scale(.9)}.banner-inner-published-form h4{color:#000;font-size:clamp(18px, 1.5vw, 26px);line-height:32px;margin:0 0 10px;text-transform:capitalize;font-weight:700;text-wrap-style:pretty}.banner-inner-published-form p{font-size:clamp(14px, 1.2vw, 16px);font-weight:500;text-align:left}.banner-inner-published-form p span{color:#00e533;font-weight:700;font-size:20px;text-decoration:underline}.banner-inner-published-form .form-group.form-group-last{margin:0}.banner-inner-published-form .form-group .form-select{font-weight:500;color:#000;border-radius:0;border:2px solid #e9e7e7;padding:10px 30px 10px 10px;cursor:pointer}.banner-inner-published-form .form-group input{background:#fbfbfb;color:#000;font-size:16px;width:100%;height:45px;border:2px solid #e9e7e7;border-radius:0;padding:10px 8px}.banner-inner-published-form .form-group input::placeholder{color:#b2b2b2;font-size:16px}.banner-inner-published-form .form-group button.submit-btn{background:linear-gradient(267.57deg,#c2f15e -3.55%,#c2f15e 100.02%);color:#000;border:none;font-size:18px;width:175px;height:45px;border-radius:5px;padding:8px 10px;margin:10px 0 0;display:flex;align-items:center;justify-content:center;transition:.3s ease-in-out}
+    </style>
         <!-- GOOGLE FONTS DEFERRED CSS -->
     <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" onload="this.onload=null;this.rel='stylesheet'"/>
     <noscript>
@@ -80,157 +85,8 @@
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K52MGDH2" height="0" width="0"
             style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
-    <header class="main-header">
-        <div class="container">
-            <nav
-                class="navbar navbar-expand-lg justify-content-center justify-content-sm-center justify-content-md-between justify-content-lg-between justify-content-xl-between justify-content-xxl-between">
-                <a class="navbar-brand" href="void:;">
-                    <img loading="lazy" src="assets/images/logo.webp" alt="logo" class="logo">
-                </a>
-                <div class="navbar-main-nav">
-                    <div class="website-main-buttons">
-                        <a class="popup-btn m-0" href="#calendary-sec">Let's Get Started</button>
-                            <a href="tel:<?php echo $locNumber; ?>" class="call-btn">Call Now <i
-                                    class="fa-solid fa-arrow-right"></i></a>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </header>
 
     <main>
-
-        <!-- Banner Main Section Start Here -->
-        <section class="banner-main-sec">
-            <picture class="bg-img">
-                <source srcset="assets/images/banner-publishing-bg.webp" type="image/webp">
-                <img src="assets/images/banner-publishing-bg.jpg" class="banner-img" alt="Banner" loading="eager" fetchpriority="high">
-            </picture>
-            <div class="container">
-                <div
-                    class="row align-items-center justify-content-between gap-3 gap-sm-3 gap-md-3 gap-lg-0 gap-xl-0 gap-xxl-0">
-                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
-                        <div class="banner-main-content" data-aos="fade-right" data-aos-duration="1500">
-                            <h1 class="heading-one">Your Voice Matters. Let’s Get It on the Page.</h1>
-                            <p>Limited time shouldn't mean your ideas go unheard. We help you transform your notes and
-                                thoughts into a professional book, making sure your story finally gets published for
-                                everyone to read.</p>
-                            <div class="website-main-buttons">
-                                <a href="#calendary-sec" class="popup-btn">Book A Discovery Call</a>
-                                <button class="chat-btn" onclick="<?php echo $locChatClick; ?>">Live Chat!</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xxl-5 col-xl-5 col-lg-5 col-md-12 col-sm-12 col-12">
-                        <!-- Banner Publishing Form Include Here -->
-                        <div class="banner-inner-published-form" data-aos="fade-left" data-aos-duration="1500">
-                            <h4>Start Your Publishing Journey!</h4>
-                                <p>Signup Now! and Get <span>35% OFF</span> Today!</p>
-                            <form action="https://leads.infinityprojectmanager.com/brand/goodspeedpublishing/lead"
-                                method="GET" class="myForm">
-                                <input type="hidden" name="city" class="city">
-                                <input type="hidden" name="country" class="country">
-                                <input type="hidden" name="ip" class="ip">
-                                <input type="hidden" name="zip_code" class="zip_code">
-                                <input type="hidden" name="brand_name" value="goodspeedpublishing.com">
-                                <input type="hidden" id="lead_area" name="lead_area"
-                                    value="https://goodspeedpublishing.com/lp/book-publishing">
-                                <div class="row">
-                                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                        <div class="form-group">
-                                            <select name="manuscript-ready" class="form-select"
-                                                aria-label="Default select example">
-                                                <option selected disabled>Do you have a manuscript ready?</option>
-                                                <option value="Yes, I'm ready to publish today">Yes, I'm ready to
-                                                    publish
-                                                    today</option>
-                                                <option value="Yes, I'll be ready to publish in 1 to 3 months">Yes, I'll
-                                                    be
-                                                    ready to publish in
-                                                    1 to 3 months</option>
-                                                <option value="Yes, I'll be ready to publish in 3 to 6 months">Yes, I'll
-                                                    be
-                                                    ready to publish in
-                                                    3 to 6 months</option>
-                                                <option value="Yes, I’ll be ready to publish in 6 to 12 months">Yes,
-                                                    I’ll be
-                                                    ready to publish in
-                                                    6 to 12 months</option>
-                                                <option value="No, I do not have a book or manuscript ready">No, I do
-                                                    not
-                                                    have a book or
-                                                    manuscript ready</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                        <div class="form-group">
-                                            <select name="published-before" class="form-select"
-                                                aria-label="Default select example">
-                                                <option selected disabled>Have you published before?</option>
-                                                <option value="Yes">Yes</option>
-                                                <option value="No">No</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                        <div class="form-group">
-                                            <select name="plan-on-publishing" class="form-select"
-                                                aria-label="Default select example">
-                                                <option selected disabled>What type of book do you plan on publishing?
-                                                </option>
-                                                <option value="Business">Business</option>
-                                                <option value="Biography">Biography</option>
-                                                <option value="Inspirational">Inspirational</option>
-                                                <option value="Non-Fiction">Non-Fiction</option>
-                                                <option value="How-To">How-To</option>
-                                                <option value="Autobiography/Memoir">Autobiography/Memoir</option>
-                                                <option value="Self-Help">Self-Help</option>
-                                                <option value="Children">Children</option>
-                                                <option value="Spiritual/Religious">Spiritual/Religious</option>
-                                                <option value="Fiction">Fiction</option>
-                                                <option value="Art/Photography">Art/Photography</option>
-                                                <option value="Cookbook">Cookbook</option>
-                                                <option value="Other">Other</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
-                                        <div class="form-group">
-                                            <input type="text" name="name" placeholder="Name:" required>
-                                        </div>
-                                    </div>
-                                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
-                                        <div class="form-group">
-                                            <input type="phone" class="countryCode" name="phone" required>
-                                            <input type="hidden" class="fullPhoneNumber" name="fullPhoneNumber">
-                                        </div>
-                                    </div>
-                                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                        <div class="form-group">
-                                            <input type="email" name="email" placeholder="Email:" required>
-                                        </div>
-                                    </div>
-                                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                        <div class="form-group">
-                                            <textarea name="message" placeholder="Tell Us A Little Bit About Your Book"
-                                                required=""></textarea>
-                                        </div>
-                                    </div>
-                                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                        <div class="form-group form-group-last">
-                                            <button type="submit" class="submit-btn">Submit</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                        <!-- Banner Publishing Form Include Here -->
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- Banner Main Section End Here -->
 
         <!-- Step Works Section Start Here -->
         <section class="step-works-wrap step-works-publishing">
